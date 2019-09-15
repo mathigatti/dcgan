7163e9070b2804d216cb1f36d51c133bf337439c
Mathias Gatti <mathigatti@gmail.com>
2019-09-15 17:01:56 -0300

    changes

--- data.py
+++ data.py
@@ -24,10 +24,10 @@ flags = FLAGS()
 
 tl.files.exists_or_mkdir(flags.checkpoint_dir) # save model
 tl.files.exists_or_mkdir(flags.sample_dir) # save generated image
-
+import os
 def get_celebA(output_size, n_epoch, batch_size):
     # dataset API and augmentation
-    images_path = tl.files.load_file_list(path='/content/data', keep_prefix=True, printable=False)
+    images_path = list(map(lambda x : '/content/data/' + x, os.listdir('/content/data')))
     def generator_train():
         for image_path in images_path:
             yield image_path.encode('utf-8')
